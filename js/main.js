2544bd31b88abf8daee28cf55da1d6e5c6464216
Gabriel Cyrillo <gabecyrillo@gmail.com>
2018-06-13 12:47:44 -0300

    Implementa Web Scraper para eventos no site do ICMC
    * Webscraper e interface de teste
    
    * Comentários e mudança no main.js
    
    Tinha uma parte do código que eu não lembro o porque estava lá. Deixei comentado pois vai que seja útil no futuro

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// Objeto para configuração do firebase, padrão da framework
+function configFirebase(){
+	var config = {
+		apiKey: "AIzaSyC85BRgXdhXnpFkqPzzA1K4PdM4Q4ApvT4",
+		authDomain: "eng-soft-f1c51.firebaseapp.com",
+		databaseURL: "https://eng-soft-f1c51.firebaseio.com",
+		projectId: "eng-soft-f1c51",
+		storageBucket: "eng-soft-f1c51.appspot.com",
+		messagingSenderId: "1041024483165"
+	};
+
+	firebase.initializeApp(config);
+}
+
+/* 
+Pega os nós contidos no nó 'events' do firebase 
+e roda a função parseEvent para cada um deles
+*/
+
+function getEvents(){
+	var db = firebase.database();
+
+    // Pega a referência do /events no firebase
+	var leadsRef = db.ref('events');
+	
+    /*
+    Eu não lembro o porque dessa coisa estar aqui, vou deixar
+    comentado pois pode ser útil no futuro 
+    */
+
+    /*
+    leadsRef.on('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+		  var childData = childSnapshot.val();
+		});
+	});
+    */
+
+    // Quando carregar um nó filho do firebase
+	leadsRef.on('child_added', function(snapshot) {
+        // Faz parse para JSON e processa a bagaça
+        parseEvent(JSON.parse(snapshot._e.T));
+	});
+}
+
+/*
+    Pega o JSON retornado do firebase e cria um HTML node a partir dele.
+    Dá append no nó #wrapper contido na página
+*/
+
+function parseEvent(json){
+    var card = document.createElement('div');
+
+    // Link do evento
+    card.href = json['href'];
+    card.target = "_blank";
+    card.classList.add('card');
+
+    // Imagem do evento
+    var img = document.createElement('img');
+    img.src = json['img'];
+    card.appendChild(img);
+
+    // Título
+    var title = document.createElement('h2');
+    title.textContent = json['title'];
+    card.appendChild(title);
+    title.classList.add('title');
+
+    // Data
+    // * Precisa fazer um parsing disso depois
+    var date = document.createElement('p');
+    date.textContent = json['date'];
+    card.appendChild(date);
+
+    // Tags do evento
+    var tags = document.createElement('p');
+    tags.textContent = "tags: " + json['tags'];
+    card.appendChild(tags);
+
+    // Enfia no HTML
+    document.querySelector('#wrapper').appendChild(card);
+}
+
+
+/* Função de setup que configura o firebase e pega os eventos */
+function setup(){
+    configFirebase();
+    getEvents();
+}
+
+// Só roda essa função quando a página estiver carregada
+window.onload = setup;
