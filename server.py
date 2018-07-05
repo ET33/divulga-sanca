d239319c0fc409fd117a56a86d4f8ab1ec2d184e
Ndrew <andre.andre831@hotmail.com>
2018-07-05 18:57:42 -0300

    Busca

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import forms
+import json 
 from webscraper.webscraper import*
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__, template_folder='templates')
 
 	
@@ -16,13 +19,38 @@ def home():
 	
 	for i in events:
 		allEvents.append(events[i])
-		
-	events = getFirebase('/events/SESC')
 	
-	for i in events:
-		allEvents.append(events[i])
     
 	return render_template('index.html', cards=allEvents)
+	
+
+with app.test_request_context('/searchresult', method='GET'):
+    assert request.path == '/searchresult'
+    assert request.method == 'GET'
+	
+	
+@app.route('/searchresult', methods=['GET'])
+def search():
+	if request.method == 'GET':
+		search = request.args.get('search', '')
+	else:
+		search = 'erro'
+	
+	
+	# events = getFirebase('/events/ICMC')
+	# allEvents = []
+		
+	# for i in events:
+		# if search in events[i]:
+			# allEvents.append(events[i])
+
+	# events = getFirebase('/events/UFSCar')
+	
+	# for i in events:
+		# if search in events[i]:
+			# allEvents.append(events[i])
+	
+	return search
 	
 
 
