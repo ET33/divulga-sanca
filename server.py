523d706d617e6330c9a00f2b6df83f048e1d53ce
Ndrew <andre.andre831@hotmail.com>
2018-07-05 13:35:01 -0300

    Flask quase funcionando

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__, template_folder='templates')
 
-# @app.route('/')
-# def hello_world():
-    # return 'Hello, World!'
 	
 @app.route('/')
 def home(name=None):
-    return render_template('index.html')
+
+	cards = []
+    
+	return render_template('index.html', cards=['1', '2', '3', '4', '1'])
