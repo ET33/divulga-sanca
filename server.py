4219f05f17b4dd5a942060d82e54acf12fc636ae
Ndrew <andre.andre831@hotmail.com>
2018-07-05 14:23:16 -0300

    Correcoes

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
+from webscraper.webscraper import*
 from flask import Flask
 from flask import render_template
 app = Flask(__name__, template_folder='templates')
 
 	
 @app.route('/')
-def home(name=None):
+def home():
+	events = getFirebase('/events/ICMC')
+	allEvents = []
 
-	cards = []
+	for i in events:
+		allEvents.append(events[i])
+
+	events = getFirebase('/events/UFSCar')
+	
+	for i in events:
+		allEvents.append(events[i])
     
-	return render_template('index.html', cards=['1', '2', '3', '4', '1'])
+	return render_template('index.html', cards=allEvents)
+	
+	
+if __name__ == '__main__':
+	app.run(debug=True)
