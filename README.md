5ffa195f23dcd309086451f0b9c203234f63c384
Ndrew <andre.andre831@hotmail.com>
2018-07-06 04:31:40 -0300

    Requests foram adicionados

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # divulga-sanca
 O projeto DivulgaSanca visa ser uma plataforma web de divulgação de eventos
+
+As tecnologias usadas pelo programa são: flask, javascript, python, html, materialize e firebase
+Algumas bibliotecas devem ser incluidas ao usar o Python:
+
+pip install bs4
+pip install certifie
+pip install chardet
+pip install idna
+pip install urld3
+pip install requests
+pip install python-firebase
+pip install workswerkug
+pip install markupsafe
+pip install jinja2
+pip install flask
+pip install itsdangerous
+pip install click
+
+rode o programa python:
+
+python server.py
--- server/server.py
+++ server/server.py
@@ -1,8 +1,13 @@
+import smtplib
 import json 
 from webscraper.webscraper import*
 from flask import Flask
 from flask import render_template
 from flask import request
+from firebase import firebase
+import datetime
+import unicodedata
+import re
 
 app = Flask(__name__, template_folder='templates')
 
@@ -33,5 +38,21 @@ def search():
 			response = "Nenhum resultado encontrado pela chave: " + key + ". Tente mais uma vez."
 			return response
 
+# @app.route('/googlesearch', methods=['GET'])
+# def gSearch():
+	# if request.method == 'GET':
+		# title = request.args.get('titulo', '')
+		# date = request.args.get('data', '')
+		
+	# return title
+	
+@app.route('/notification', methods=['GET'])
+def notification():
+	
+	email = request.args.get('email', '')
+	
+	return email
+
+	
 if __name__ == '__main__':
 	app.run(debug=True)
